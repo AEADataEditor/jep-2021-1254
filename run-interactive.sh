ecca22290be50786ffb6cf309cd0f65667af6b42
Lars Vilhuber <lars.vilhuber@cornell.edu>
2022-03-23 16:31:48 +0000

    Adapting

---
+++ run-interactive.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [[ -f config.txt ]]
+then 
+   configfile=config.txt
+else 
+   configfile=init.config.txt
+fi
+
+# name of the file to run
+file=code/main.do
+
+
+echo "================================"
+echo "Pulling defaults from ${configfile}:"
+cat $configfile
+echo "--------------------------------"
+
+source $configfile
+
+echo "================================"
+echo "Running docker:"
+set -ev
+
+DOCKEROPTS=" --rm -u 0 "
+DOCKERIMG=$(echo $MYHUBID/$MYIMG | tr [A-Z] [a-z])
+
+# ensure that the directories are writable by Docker
+
+
+# run the docker 
+
+time docker run $DOCKEROPTS \
+  -v ${STATALIC}:/usr/local/stata/stata.lic \
+  -v $(pwd):/home/rstudio/project \
+  -e DISABLE_AUTH=true \
+  -p 8787:8787 \
+  --entrypoint /init \
+  $DOCKERIMG:$TAG "$@"
