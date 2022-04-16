c5045ab9d727a60353ebbe63027facf3b14f1c43
Lars Vilhuber <lars.vilhuber@cornell.edu>
2022-04-16 23:43:08 +0000

    Adding devcontainer

---
+++ .devcontainer/init.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+ if [ ! -f stata.lic ]
+ then
+    if [ -z ${STATA_LIC_BASE64} ]
+    then
+        echo "No license found."
+    else
+        echo "${STATA_LIC_BASE64}" | base64 -d > stata.lic 
+    fi
+fi
+echo "init done."
