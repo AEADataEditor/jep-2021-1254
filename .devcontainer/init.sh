d7a115300d2fa54d156817df8f29c90a073f2520
Lars Vilhuber <lars.vilhuber@cornell.edu>
2022-04-17 19:53:30 +0000

    Added rclone

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -8,4 +8,7 @@ if [ ! -f stata.lic ]
         echo "${STATA_LIC_BASE64}" | base64 -d > stata.lic 
     fi
 fi
+# Install rclone
+curl https://rclone.org/install.sh | sudo bash
+# done.
 echo "init done."
